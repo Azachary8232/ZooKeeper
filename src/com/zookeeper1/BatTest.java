43b675c99996b953132abc39711db0bbcc3429da
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-12-01 14:43:56 -0700

    Added BatTest File

---
+++ src/com/zookeeper1/BatTest.java
@@ -0,0 +1,26 @@
+package com.zookeeper1;
+
+public class BatTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	Bat bat1 = new Bat();
+	bat1.displayEnergy();
+	bat1.fly();
+	bat1.fly();
+	bat1.displayEnergy();
+	bat1.eatHumans();
+	bat1.eatHumans();
+	bat1.displayEnergy();
+	bat1.attackTown();
+	bat1.attackTown();
+	bat1.attackTown();
+	bat1.displayEnergy();
+
+	
+	
+	}
+	
+}
